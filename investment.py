891a61066be21941cfc4e7177870d273c799eaf5
Ziyang Wang <ziw152@ucsd.edu>
2019-02-21 10:54:38 -0800

    Update investment.py

--- investment.py
+++ investment.py
@@ -1,1 +1,13 @@
+!/usr/bin/env python3
+amount = float(input("Enter the amount: ")) #get amount of money 
+inrate = float(input("Enter the interest rate: ")) # get the interest rate 
+period = int(input("Enter the time period: "))     #get the period of saving 
 
+#check and calculate 
+value = 0          
+year = 1           
+while year <= period:                            
+    value = amount + (inrate * amount)           
+    print("year {} Rs. {:.2f}”.format(year,value))
+    amount = value 
+    year = year + 1
